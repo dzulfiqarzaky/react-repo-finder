73ac8a1ef8a39af01227d73248d1d8f345beb38c
dzulfiqarzaky <dzulfiqarzakyaulia@gmail.com>
2023-07-04 02:03:39 +0700

    add a dropdown logic, color logic, handling 404 error and empty repos, optimize api fetching logic so it will not do another fetch if the data is available

--- src/App.js
+++ src/App.js
@@ -2,52 +2,67 @@ import './App.css';
 import axios from 'axios';
 import { useState } from 'react';
 
-function Input(props) {
-  return (
-    <input type="text" onChange={props.onChange} value={props.value} />
-  );
-}
-
 function App() {
   const [githubRepos, setGithubRepos] = useState([]);
   const [githubReposSearch, setGithubReposSearch] = useState('');
   const [showTextSearch, setShowTextSearch] = useState('');
   const [userReposList, setUserReposList] = useState([]);
   
-  const searchRepos = (e) => {
-    e.preventDefault();
-    setShowTextSearch(githubReposSearch);
-    axios.get(`https://api.github.com/search/repositories?q=${githubReposSearch}&page=1&per_page=10&sort=stars&order=desc`)
-      .then(res => {
-        setGithubRepos(res.data.items);
-      })
-      .catch(err => {
-        console.log(err);
-      });
-  };
+const searchRepos = async (e) => {
+  e.preventDefault();
+  setShowTextSearch(githubReposSearch);
+  setUserReposList([]);
+  try {
+    const response = await axios.get(`https://api.github.com/search/repositories`, {
+      params: {
+        q: githubReposSearch,
+        page: 1,
+        per_page: 10,
+        sort: 'stars',
+        order: 'desc'
+      }
+    });
+    setGithubRepos(response.data.items);
+  } catch (error) {
+    console.log(error);
+  }
+};
   
-  const getUserRepos = (e, name, id) => {
-    e.preventDefault();
-    axios.get(`https://api.github.com/users/${name}/repos`)
-      .then(res => {
-        if (userReposList.length === 0) {
-          setUserReposList([{ data: res.data, id }]);
-        } else {
-          const foundId = userReposList.findIndex(repo => repo.id === id);
-          if (foundId !== -1) {
-            setUserReposList([...userReposList, { data: res.data, id }]);
-          }
-        }
-      })
-      .catch(err => {
-        console.log(err);
-      });
-  };
-  
+const getUserRepos = async (e, name, id) => {
+  e.preventDefault();
+  const foundId = userReposList?.findIndex(repo => repo.id === id);
+  if (foundId !== -1 && userReposList.length !== 0) {
+    const updatedReposList = [...userReposList];
+    updatedReposList[foundId].open = !updatedReposList[foundId].open;
+    setUserReposList(updatedReposList);
+  } else {
+    try {
+      const res = await axios.get(`https://api.github.com/users/${name}/repos`);
+      const data = res.data.length !== 0 ? res.data : [{
+        name: "Not Found",
+        description: "No Repos Found",
+        stargazers_count: "no repos"
+      }];
+      const color = res.data.length !== 0 ? "green" : "yellow";
+      const updatedReposList = [...userReposList, { data, id, open: true, color }];
+      setUserReposList(updatedReposList);
+    } catch (err) {
+      const data = [{
+        name: err.code,
+        description: err.message,
+        stargazers_count: "error"
+      }];
+      const updatedReposList = [...userReposList, { data, id, open: true, color: "red" }];
+      setUserReposList(updatedReposList);
+    }
+  }
+};
+
   return (
     <div className="App">
       <form onSubmit={searchRepos}>
-        <Input 
+        <input
+          type="text" 
           onChange={(e) => setGithubReposSearch(e.target.value)} 
           value={githubReposSearch} 
         />
@@ -58,9 +73,11 @@ function App() {
         {githubRepos.map((repo, idx) => (
           <div key={repo.id}>
             <h3>{repo.name}</h3>
-            <button onClick={(e) => getUserRepos(e, repo.name, idx)}>view repos</button>
-            {userReposList.length > 0 && userReposList[idx] && userReposList[idx].data.map(repo => (
-              <div key={repo.id} style={{ background: 'red' }}>
+            <button onClick={(e) => {
+              getUserRepos(e, repo.name, idx)
+            }}>view repos</button>
+          {userReposList.length > 0 && userReposList?.filter(repo => repo.id === idx && repo.open)[0]?.data.map(repo => (
+              <div key={repo.id} style={{ background: userReposList[idx].color }}>
                 <h3>{repo.name}</h3>
                 <p>{repo.description}</p>
                 <p>stars: {repo.stargazers_count}</p>
