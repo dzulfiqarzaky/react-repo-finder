17c4f599d203f1220452624949dd5ae94d0c807e
dzulfiqarzaky <dzulfiqarzakyaulia@gmail.com>
2023-07-05 11:56:08 +0700

    add styled component for search form

--- src/SearchForm.js
+++ src/SearchForm.js
@@ -1,19 +1,35 @@
 import React from 'react';
 import { Input, Button } from 'antd';
+import styled from 'styled-components';
+
+const FormContainer = styled.form`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+`;
+
+const StyledInput = styled(Input)`
+  width: 100%;
+  margin-bottom: 10px;
+`;
+
+const StyledButton = styled(Button)`
+  width: 100%;
+`;
 
 const SearchForm = ({ onSubmit, value, onChange }) => {
   return (
-    <form onSubmit={onSubmit}>
-      <Input
+    <FormContainer onSubmit={onSubmit}>
+      <StyledInput
         size="large"
         onChange={onChange}
         value={value}
         placeholder="Enter username"
       />
-      <Button type="primary" htmlType="submit" size="large">
+      <StyledButton type="primary" htmlType="submit" size="large">
         Search
-      </Button>
-    </form>
+      </StyledButton>
+    </FormContainer>
   );
 };
 
